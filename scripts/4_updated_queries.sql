bf32169760644e643ca2d1471ccc86992dc8ed1d
Pavel Belikov <pbelikov@gmail.com>
2018-10-16 13:49:18 +0300

    Revert it all

--- scripts/4_updated_queries.sql
+++ scripts/4_updated_queries.sql
@@ -1,11 +1,11 @@
 -- 1) Get all the foods which names contain partial
 SELECT food_name
 FROM foods
-WHERE :partial_food_name LIKE food_name || '%';
+WHERE food_name LIKE :partial_food_name || '%';
 
 -- 2) Get all the eaters, that ate something and whose names contain partial
 SELECT e.eater_name, f.food_name
 FROM eaters e
      LEFT JOIN foods f ON f.id = e.food_id
-WHERE :partial_eater_name LIKE UPPER (e.eater_name) || '%'
+WHERE UPPER (e.eater_name) LIKE :partial_eater_name || '%'
   AND NVL (f.food_name, 'not eaten') <> 'not eaten';
